01c49a1fb4ac74b37cc94c28bc7f638ea838d4a0
ericliudx <ericliudx@gmail.com>
2025-09-04 20:36:05 +0000

    changes to news_agent

--- news_agent.py
+++ news_agent.py
@@ -1,19 +1,38 @@
 import requests
 from transformers import pipeline
 
-summarizer = pipeline("summarization", model = "facebook/bart-large-cnn")
 
-def fetch_news (api_key, query="technology"):
-  #fetching news articles with NewsAPI
-  url = f"https://newsapi.org/v2/everything?q={query}&apiKey={api_key}"
-  response = requests.get(url)
-  return response.json().get("articles", [])
+class NewsAgent:
+  def __init__(self, api_key, query="technology", model="facebook/bart-large-cnn"):
+    self.api_key = api_key
+    self.summarizer = pipeline("summarization", model=model)
+    self.query = query
 
-def summarize_article (text):
-  if not text:
-    return "No content available"
-  if len(text) < 100:
-    return text
-  summary = summarizer(text, max_length=100, min_length=30, do_sample=False)
-  print(summary)
-  return summary[0]['summary_text'] 
+  def fetch_news (api_key, query="technology"):
+    #fetching news articles with NewsAPI
+    url = f"https://newsapi.org/v2/everything?q={query}&apiKey={api_key}"
+    response = requests.get(url)
+    return response.json().get("articles", [])
+
+  def summarize_article (self, text):
+    if not text:
+      return "No content available"
+    if len(text) < 100:
+      return text
+    summary = self.summarizer(text, max_length=100, min_length=30, do_sample=False)
+    print(summary[0])
+    return summary[0]['summary_text'] 
+
+  def get_summaries(self, query, limit = 5):
+    articles = self.fetch_news(query)
+    results = []
+    for article in articles[:limit]:
+      content = article.get("content") or article.get("description")
+      summary = self.summarize.content()
+      results.append({
+        "title": article["title"],
+        "url": article["url"],
+        "summary": summary
+      })
+      return results
+
